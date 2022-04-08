eba0a517b468c892d723cacc3903115ca957cc2d
J. Austin Tsiaperas <liketexas@gmail.com>
2022-04-08 11:57:13 -0500

    further testing of mongodb. getting 200 response from post but nothing seems to happen

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,33 +6,14 @@ module.exports = function(app){
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
 
-    app.get("/api/register", function(req,res){
+    app.get("/api/user", function(req,res){
         res.json(friends.friendsList);
     });
     
-    app.post("/api/register", function(req,res){
+    app.post("/api/user", function(req,res){
         console.log(req.body);
-        //var newUser = new friends.Friend(req.body.name,req.body.photo,req.body.email);
-        // using Twilio SendGrid's v3 Node.js Library
-        // https://github.com/sendgrid/sendgrid-nodejs
-        const sgMail = require('@sendgrid/mail')
-        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-        const msg = {
-            to: req.body.email, // Change to your recipient
-            from: 'jatsiaperas@gmail.com', // Change to your verified sender
-            subject: 'M1 interactive registration',
-            text: 'has been completed',
-            html: '<strong>has been completed</strong>',
-        }
-        sgMail
-        .send(msg)
-        .then(() => {
-            console.log('Email sent')
-        })
-        .catch((error) => {
-            console.error(error)
-        })
-        res.json({"message":"Registered!"});
+        
+        res.json(res.body);
     });
 
 };
--- server.js
+++ server.js
@@ -4,26 +4,27 @@ const express = require("express");
 const path = require("path");
 const mysql = require('mysql');
 const app = express();
-const cors = require('cors');
 const mongoose = require("mongoose");
-
+const bodyParser= require("body-parser");
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
 require('dotenv').config();
 
-app.use(cors());
+//app.use(cors());
 // Sets up the Express App
 // =============================================================
 
 const port = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
+
 const db = require("./models");
 
 //set mongoDB for local or heroku
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/cms-test";
 
-
+// Use body-parser for handling form submissions
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -31,7 +32,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-app.get("/api/user/", /*authCheck,*/ function(req,res){
+app.get("/api/user/", function(req,res){
 
 	db.User.find()
 	.then(users => res.json(users))
@@ -39,11 +40,31 @@ app.get("/api/user/", /*authCheck,*/ function(req,res){
 });
 
 
-app.post("/api/user", /*authCheck,*/ function(req,res){
-  console.log(user);
-	db.User.findOneAndUpdate({email: user.email},{email: user.email, name: user.name, picture: user.picture},{upsert: true})
+app.post("/api/user", function(req,res){
+	db.User.create(req.body)
 	.then(user => res.json(user))
 	.catch(err => res.json(err));
+
+  
+        // using Twilio SendGrid's v3 Node.js Library
+        // https://github.com/sendgrid/sendgrid-nodejs
+        const sgMail = require('@sendgrid/mail')
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+        const msg = {
+            to: req.body.email, // Change to your recipient
+            from: 'jatsiaperas@gmail.com', // Change to your verified sender
+            subject: 'M1 interactive registration',
+            text: 'has been completed',
+            html: '<strong>has been completed</strong>',
+        }
+        sgMail
+        .send(msg)
+        .then(() => {
+            console.log('Email sent')
+        })
+        .catch((error) => {
+            console.error(error)
+        })
 });
 
 mongoose.connect(MONGODB_URI).then(
