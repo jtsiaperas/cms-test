c577f2997c5b538558acd44a197d9a3612f96c21
J. Austin Tsiaperas <liketexas@gmail.com>
2022-04-08 13:38:30 -0500

    fixed vulnerability

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,14 +5,14 @@ var path = require("path");
 module.exports = function (app){
     const { auth } = require('express-openid-connect');
 
-    const config = {
-      authRequired: true,
-      auth0Logout: true,
-      secret: process.env.SECRET || "Pjmgqokja6_SSFVy0qOXnWaExlp4WSMp5urN1MwtTinUrble69KxAZBb7eqOuMUJ",
-      baseURL: 'http://localhost:3000',
-      clientID: 'iQ7y6Vj496fwTmUaARdE1CVDmt0f2BaY',
-      issuerBaseURL: 'https://odakotalogin.auth0.com'
-    };
+    // const config = {
+    //   authRequired: true,
+    //   auth0Logout: true,
+    //   secret: process.env.SECRET,
+    //   baseURL: 'http://localhost:3000',
+    //   clientID: 'iQ7y6Vj496fwTmUaARdE1CVDmt0f2BaY',
+    //   issuerBaseURL: 'https://odakotalogin.auth0.com'
+    // };
     
     // auth router attaches /login, /logout, and /callback routes to the baseURL
     //app.use(auth(config));
